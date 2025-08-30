0c1e17d77c16084278653eaba0e028351a57afb2
Abdelhadi <abdelhadibahddou@gmail.com>
2025-08-30 11:45:18 +0100

    Car listing UI with sample data.

--- app/src/main/java/com/capgemini/carcatalog/MainActivity.kt
+++ app/src/main/java/com/capgemini/carcatalog/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.capgemini.carcatalog.ui.theme.CarCatalogTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +30,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             CarCatalogTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    LazyColumn(
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                        contentPadding = PaddingValues(
+                            start = 16.dp,
+                            top = 16.dp,
+                            end = 16.dp,
+                            bottom = 56.dp
+                        ),
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        items(20) { index ->
+                            CarItem()
+                        }
+                    }
                 }
             }
         }
@@ -31,17 +51,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CarCatalogTheme {
-        Greeting("Android")
+fun CarItem() {
+    Row {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier.size(56.dp)
+        )
+        Spacer(Modifier.width(16.dp))
+        Column {
+            Text(text = "Car name")
+            Text(text = "Car description")
+        }
     }
 }
