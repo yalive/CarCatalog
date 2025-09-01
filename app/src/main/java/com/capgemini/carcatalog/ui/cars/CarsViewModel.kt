8a88502cb53c25c63b536eafc4a1b5ba44aef63a
Abdelhadi <abdelhadibahddou@gmail.com>
2025-09-01 09:50:15 +0100

    Refactor ViewModel: Review data loading by switching from viewmodel init approach to a flow based solution as it has more advantages (data refresh control unlike init which load data once, easier unit testing of the viewmodel...)

--- app/src/main/java/com/capgemini/carcatalog/ui/cars/CarsViewModel.kt
+++ app/src/main/java/com/capgemini/carcatalog/ui/cars/CarsViewModel.kt
@@ -5,8 +5,13 @@ import androidx.lifecycle.viewModelScope
 import com.capgemini.carcatalog.common.Result
 import com.capgemini.carcatalog.domain.GetCarsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,44 +20,59 @@ class CarsViewModel @Inject constructor(
     private val getCars: GetCarsUseCase,
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<CarsUiState>(CarsUiState.Loading)
-    val uiState: StateFlow<CarsUiState> = _uiState
+    private sealed interface UserAction {
+        object Retry : UserAction
+        object Refresh : UserAction
+    }
 
-    init {
-        prepareCarCatalog()
+    private val userActions = MutableSharedFlow<UserAction>()
+    private var currentState: CarsUiState = CarsUiState.Loading
+
+    val uiState: StateFlow<CarsUiState> = flow {
+        if (currentState is CarsUiState.Data) {
+            // Do not hit api again when data is already loaded and emit cached data
+            emit(currentState)
+        } else {
+            emit(CarsUiState.Loading)
+            emit(getCarsState())
+        }
+        userActions.collect { event ->
+            when (event) {
+                UserAction.Refresh -> {
+                    val dataState = currentState as? CarsUiState.Data
+                    val refreshingState = dataState?.copy(isRefreshing = true)
+                    emit(refreshingState ?: CarsUiState.Loading)
+                    emit(getCarsState())
+                }
+
+                UserAction.Retry -> {
+                    emit(CarsUiState.Loading)
+                    emit(getCarsState())
+                }
+            }
+        }
+    }.distinctUntilChanged().onEach {
+        // cache current state
+        currentState = it
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(0),
+        initialValue = currentState
+    )
+
+    private suspend fun getCarsState() = when (val result = getCars()) {
+        is Result.Error -> CarsUiState.Error
+        is Result.Success -> CarsUiState.Data(
+            cars = result.data.map { it.toCarUiModel() },
+            isRefreshing = false
+        )
     }
 
     fun refresh() {
-        val uiData = _uiState.value as? CarsUiState.Data ?: return
-        if (uiData.isRefreshing) return
-        _uiState.value = uiData.copy(isRefreshing = true)
-        viewModelScope.launch {
-            val result = getCars()
-            val state = when (result) {
-                is Result.Error -> uiData
-                is Result.Success -> CarsUiState.Data(
-                    cars = result.data.map { it.toCarUiModel() },
-                    isRefreshing = false
-                )
-            }
-            _uiState.value = state
-        }
+        viewModelScope.launch { userActions.emit(UserAction.Refresh) }
     }
 
     fun retry() {
-        prepareCarCatalog()
-    }
-
-    private fun prepareCarCatalog() = viewModelScope.launch {
-        _uiState.value = CarsUiState.Loading
-        val result = getCars()
-        val state = when (result) {
-            is Result.Error -> CarsUiState.Error
-            is Result.Success -> CarsUiState.Data(
-                cars = result.data.map { it.toCarUiModel() },
-                isRefreshing = false
-            )
-        }
-        _uiState.value = state
+        viewModelScope.launch { userActions.emit(UserAction.Retry) }
     }
 }
